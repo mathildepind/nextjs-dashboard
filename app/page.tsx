183b189d02966c3b4841b2b973f7a7eb0ce395c3
Mathilde Pind Rasmussen <mathilde@mathildepind.com>
2025-01-31 15:18:02 -0800

    add metdadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
+import { Metadata } from "next";
+
+export const metadata: Metadata = { title: "Dashboard" };
 
 export default function Page() {
   return (
